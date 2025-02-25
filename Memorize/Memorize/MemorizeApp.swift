8cd9fe96b29051ff2871b22181d228ccc3a2d7ae
Jeffrey Woodbury <jeffrey.woodbury@byu.edu>
2025-02-25 16:53:00 -0700

    Initial commit Setup the CardView and ContentView

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jeffrey Woodbury on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
